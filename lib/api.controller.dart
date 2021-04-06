bf870715f032e42808f5a66bd7af76bc7908e6c3
lancelynyrd <pinedaclp@gmail.com>
2021-04-07 02:38:16 +0800

    debug

--- lib/api.controller.dart
+++ lib/api.controller.dart
@@ -331,8 +331,8 @@ class Api {
 
     try {
       String queryString = Uri(queryParameters: params).query;
-      print("_printDebugUrl: $apiUrl?$queryString");
-      debugPrint("_printDebugUrl: $apiUrl?$queryString", wrapWidth: 1024);
+      // print("_printDebugUrl: $apiUrl?$queryString");
+      // debugPrint("_printDebugUrl: $apiUrl?$queryString", wrapWidth: 1024);
     } catch (e) {
       print("Caught error on _printDebug() with data: ");
       print(data);
