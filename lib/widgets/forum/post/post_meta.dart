8ce84e75aef8add28fa3d6328957d17d2b9d9feb
JAEHO SONG <thruthesky@gmail.com>
2021-04-13 21:59:41 +0900

    update for user avatar

--- lib/widgets/forum/post/post_meta.dart
+++ lib/widgets/forum/post/post_meta.dart
@@ -44,18 +44,18 @@ class PostMeta extends StatelessWidget {
             Text('${post.comments.length}', style: TextStyle(fontSize: Space.xsm)),
             SizedBox(width: Space.xsm),
           ],
-          if (forum.showLike && post.y.toInt > 0) ...[
-            Icon(Icons.thumb_up_rounded, size: Space.xsm, color: iconColor),
-            SizedBox(width: Space.xs),
-            Text('${post.y}', style: TextStyle(fontSize: Space.xsm)),
-            SizedBox(width: Space.xsm),
-          ],
-          if (forum.showDislike && post.n.toInt > 0) ...[
-            Icon(Icons.thumb_down_rounded, size: Space.xsm, color: iconColor),
-            SizedBox(width: Space.xs),
-            Text('${post.n}', style: TextStyle(fontSize: Space.xsm)),
-            SizedBox(width: Space.xsm),
-          ],
+          // if (forum.showLike && post.y.toInt > 0) ...[
+          //   Icon(Icons.thumb_up_rounded, size: Space.xsm, color: iconColor),
+          //   SizedBox(width: Space.xs),
+          //   Text('${post.y}', style: TextStyle(fontSize: Space.xsm)),
+          //   SizedBox(width: Space.xsm),
+          // ],
+          // if (forum.showDislike && post.n.toInt > 0) ...[
+          //   Icon(Icons.thumb_down_rounded, size: Space.xsm, color: iconColor),
+          //   SizedBox(width: Space.xs),
+          //   Text('${post.n}', style: TextStyle(fontSize: Space.xsm)),
+          //   SizedBox(width: Space.xsm),
+          // ],
           Text(
             'No. ${post.idx}',
             style: TextStyle(fontSize: Space.xsm),
