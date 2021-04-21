903c24be2069c342726eede123b3b79ae7566e4f
JAEHO SONG <thruthesky@gmail.com>
2021-04-21 12:45:35 +0900

    aip conroller

--- lib/api.controller.dart
+++ lib/api.controller.dart
@@ -351,7 +351,7 @@ class Api {
 
     dynamic res;
     try {
-      // _printDebugUrl(data);
+      _printDebugUrl(data);
       res = await dio.post(apiUrl, data: data);
     } catch (e) {
       print('dio.post() got error; apiUrl: $apiUrl');
