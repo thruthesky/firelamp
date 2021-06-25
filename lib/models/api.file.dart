8e6c3c3d06063b1b8cbcb24d608cfbdcdf096482
JaeHo Song <thruthesky@gmail.com>
2021-06-25 20:52:49 +0900

    update type error

--- lib/models/api.file.dart
+++ lib/models/api.file.dart
@@ -32,8 +32,8 @@ class ApiFile {
     // String url = Api.instance.thumbnailUrl;
     // url = url + '?src=${json['idx']}&w=360&h=360&f=jpeg&q=95';
 
-    String url =
-        Api.instance.thumbnailUrl(src: json['idx'], width: 480, quality: 95, original: true);
+    String url = Api.instance
+        .thumbnailUrl(src: json['idx'].toString(), width: 480, quality: 95, original: true);
 
     // print('url: thumbnail: $url');
     return ApiFile(
